31168c2c05688a4bc0cac047ffa43b22f000b10b
kouame590 <kouadio.kouame@jjay.cuny.edu>
2021-03-02 19:07:09 +0000

    Lab3

--- main.cpp
+++ main.cpp
@@ -7,6 +7,64 @@
 // Put the assignment code here
 
 class Quaternion {
+
+ public: 
+ 
+ double a,b,c,d;
+
+  quaternoon(double p,double q,double r,double s){
+    a=p;
+    b=q;
+    c=r;
+    d=s;
+
+  }
+  bool operatpor ==(const quaternion p){
+    if ((p->a==this-a)&&(p->b==this->b)&&(p->c==this->c)&&
+    (p->d==this->d))
+    return true;
+
+else 
+    bool operatpor ==(const quaternion p){
+    if ((p->a==this-a)&&(p->b==this->b)&&(p->c==this->c)&&
+    (p->d==this->d))
+    return false;
+  
+  }
+  quaternion operator+(const quaternion)
+  {
+    quaternion r;
+    r.a=p.a+this->a;
+    r.b=p.b+this->b;
+    r.c=p.c+this->c;
+    r.d=p.d+this->d;
+
+    return r;
+  }
+  quaternion operator -(const quaternion p){
+    quaternion r;
+
+    r.a=p-a-this->a;
+    r.b=p-b-this->b;
+    r.c=p.c-this->c;
+    r.d=p.d-this->d
+
+    return r;
+  }
+    quaternion operator*(double s){
+      quaternion p;
+  
+    p.a=s*this->a;
+    p.b=s*this->b;
+    p.c=s.c*this->c;
+    p.d=s.d*this->d;
+
+    return p;
+    }
+
+  }
+
+
 };
 
 //------------------------------
